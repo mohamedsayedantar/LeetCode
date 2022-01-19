9dca6f7732c55c68bdfc8d13a1636ab194d0e34c
Mohamed Sayed <mohamedsayed99.ms@gmail.com>
2022-01-19 10:28:58 +0200

    Time: 58 ms (38.24%), Space: 16.5 MB (41.45%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -6,24 +6,19 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        
         self.paths = []
         def sum_path(root, path):
-            #print(root.val, path)
             if root:
                 if root.left or root.right:
-                    path += str(root.val) + " "
-                    #print(path)
-                    sum_path(root.left, path)
-                    sum_path(root.right, path)
+                    #path += str(root.val) + " "
+                    sum_path(root.left, path+ str(root.val) + " ")
+                    sum_path(root.right, path+ str(root.val) + " ")
                 else:
                     path += str(root.val) + " "
-                    #print("hereeeee", path.split())
                     list1 = []
                     for i in path.split():
                         list1.append(int(i))
                     if sum(list1)==targetSum:
-                        #print("kkkk", sum(list1))
                         self.paths.append(list1)
                     
         sum_path(root, "")
