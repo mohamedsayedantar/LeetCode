2871af007c15c051206097620166521101fb7ef3
Mohamed Sayed <mohamedsayed99.ms@gmail.com>
2022-01-19 10:25:45 +0200

    Time: 79 ms (9.39%), Space: 15.3 MB (11.67%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -8,18 +8,14 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         self.state = False
         def sum_path(root, sum1):
-            #print(root.val, sum1)
             if root:
                 if root.left or root.right:
-                    sum1 += root.val
-                    sum_path(root.left, sum1)
-                    sum_path(root.right, sum1)
+                    #sum1 += root.val
+                    sum_path(root.left, sum1+root.val)
+                    sum_path(root.right, sum1+root.val)
                 else:
-                    
                     sum1 += root.val
-                    #print("hereeeee", sum1)
                     if sum1==targetSum:
-                        #print("kkkk")
                         self.state = True
                     
         sum_path(root, 0)
