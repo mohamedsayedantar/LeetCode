b2c34b287be08e88f25a3863022f268d94d0b869
Mohamed Sayed <mohamedsayed99.ms@gmail.com>
2022-01-26 22:17:13 +0200

    Time: 24 ms (96.31%), Space: 14.1 MB (66.86%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
@@ -1,7 +1,3 @@
 class Solution:
     def isSameAfterReversals(self, num: int) -> bool:
-        def rev(n):
-            return int(str(n)[::-1])
-        
-        R2 = rev(rev(num))
-        return num==R2 
+        return num==int(str(int(str(num)[::-1]))[::-1]) 
