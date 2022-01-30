cd55be99b6b365a4074cbe0ec878d03bd84b0d32
Mohamed Sayed <mohamedsayed99.ms@gmail.com>
2022-01-30 11:18:40 +0200

    Time: 38 ms (41.22%), Space: 14 MB (96.05%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.py
+++ 461-hamming-distance/461-hamming-distance.py
@@ -1,20 +1,12 @@
 import math
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        X = str(bin(x)[2:])
-        Y = str(bin(y)[2:])
-        X_len = len(X)
-        Y_len = len(Y)
+        xor = x ^ y
+        print(xor)
+        count = 0
+        for _ in range(32):
+            count += xor & 1
+            xor = xor >> 1
+        return count
         
-        if X_len>Y_len:
-            Y = str(0)*(X_len-Y_len) + Y
-        else:
-            X = str(0)*(Y_len-X_len) + X
-        
-        out = 0
-        for i in range(len(X)):
-            if X[i] != Y[i]:
-                out += 1
-                
-        return out
         
