aec303c700b0b8792f86a18c0c494cb790b5b38a
Mohamed Sayed <mohamedsayed99.ms@gmail.com>
2022-10-04 20:01:11 +0200

    Time: 330 ms (28.82%), Space: 15.9 MB (6.62%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,13 +1,12 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         L = len(nums)
-        dict1 = {}
+        myset = set()
         for i in range(L+1):
-            dict1[i] = "F"
+            myset.add(i)
             
         for num in nums:
-            dict1[num] = "T"
+            myset.remove(num)
         
-        for num in dict1:
-            if dict1[num] == "F":
-                return num
+        for num in myset:
+            return num
